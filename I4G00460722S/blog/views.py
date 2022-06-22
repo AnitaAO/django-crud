5c9c4165a3e12cc5214ddd57bdd6dc6e4dbe5780
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-06-22 13:16:19 +0100

    update

--- I4G00460722S/blog/views.py
+++ I4G00460722S/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from msilib.schema import ListView
+from django.urls import reverse_lazy
+
+from blog.models import Post
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    
+class PostCtreateView(CreateView):
+    model: Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostUpdateView(UpdateView):
+    model: Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+    
+class PostDeleteView(DeleteView):
+    model: Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+    
